a6ee2e63b12ad951cd64366b41ab7ce39e8cadac
aleyalunin <ale.yalunin@gmail.com>
2017-02-20 21:24:55 +0300

    Changed name of folder and restored color variable

--- HSEapp/AppDelegate.swift
+++ HSEapp/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 var hseColor = UIColor(red: 0/255.0, green: 71/255.0, blue: 136/255.0, alpha: 1.0)
+var hseColorPassive = UIColor(red: 128/255.0, green: 128/255.0, blue: 128/255.0, alpha: 1.0)
 var headerColor = UIColor(red: 216/255.0, green: 216/255.0, blue: 216/255.0, alpha: 1.0)
 
 @UIApplicationMain
